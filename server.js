35f47f0dfaddf1baf94910f9acf48a732bd79513
raingrave <raintech@outlook.com>
2019-12-08 23:46:17 -0300

    create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const bodyParser = require('body-parser')
+
+const routes = require('./src/routes/index.js')
+
+require('dotenv').config()
+
+require('./src/database')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use(routes)
+
+app.listen(9090, function () {
+  console.log('start on port 9090!');
+});

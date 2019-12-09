faf97bf4bce9b4fbcf5deddd45641d5dc5ee45be
raingrave <raintech@outlook.com>
2019-12-08 23:54:13 -0300

    create jwt service

---
+++ src/services/jwtService.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+
+exports.generateToken = (payload) => {
+	return jwt.sign({
+  		data: payload
+	}, process.env.JWT_SECRET, {
+		algorithm: process.env.JWT_ALGORITHM,
+		expiresIn: process.env.JWT_LIFECICLE
+	})
+}
+
+exports.tokenVerify = (token) => {
+	try {
+		return jwt.verify(token, process.env.JWT_SECRET).id
+	} catch(error) {
+		return error.message
+	}
+}

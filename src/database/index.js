ecc081a68302549df2d5f49101382bb11f77b49e
raingrave <raintech@outlook.com>
2019-12-09 23:54:40 -0300

    added movies routes

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,15 @@
 const Sequelize = require('sequelize')
 
-const connection = new Sequelize(require('../config/database.js'))
+const dbConfig = require('../config/database.js')
+
+const connection = new Sequelize(dbConfig)
 
 const Customer = require('../models/Customer.js')
 
+const Movie = require('../models/Movie.js')
+
 Customer.init(connection)
+
+Movie.init(connection)
 
 module.exports = connection
